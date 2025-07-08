716b8dbac3e5cf7910d8d0d083ccce1f364c7a39
Kingsley Osei <kingsley.osei@outlook.com>
2025-07-08 01:07:57 +0000

    production ready release for referral section

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { registerLicense } from '@syncfusion/ej2-base';
 import App from './App.tsx'
 import './index.css'
-
-registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
